67f19883e44769662a6254320ff10bb7a75ba8bd
Noel del Castillo <ngdelcastillo@gmail.com>
2015-07-08 21:45:03 +0800

    Adds component for new messages

--- app/routes/channels/show.js
+++ app/routes/channels/show.js
@@ -4,18 +4,19 @@ export default Ember.Route.extend({
   model(params){
     return this.store.find('channel', params['id']).then((channel) => {
       return Ember.RSVP.hash({
-        channel: channel
+        channel: channel,
+        messages: channel.get('messages')
       });
     });
   },
   setupController(controller, model){
     controller.set('channel', model.channel);
-    controller.set('messages', model.channel.get('messages'));
+    controller.set('messages', model.messages);
   },
   actions: {
     createMessage(params){
       params.sender = this.get('session.secure.email');
-      params.timestamp = new Date().getTime()
+      params.timestamp = new Date().getTime();
       let message = this.store.createRecord('message', params);
       let channel = this.modelFor('channels.show').channel;
       channel.get('messages').addObject(message);
